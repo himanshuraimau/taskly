f19d2daab1331fe9889686bbfaa3f69f803c1f91
Himanshu Rai <himanshuraimau9@gmail.com>
2024-10-12 12:30:29 +0530

    create button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Alert,TouchableOpacity } from 'react-native';
+import { theme } from './theme';
 
 export default function App() {
+     
+  const handleDelete = () => {
+    Alert.alert('Delete','Are you sure you want to delete this item?',[
+      {
+        text:'Yes',
+        onPress:()=>console.log('Delete Pressed'),
+        style:'destructive'
+      },
+      {
+        text:'No',
+        onPress:()=>console.log('Cancel Pressed'),
+        style:'cancel'
+      }
+    ])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+      
+
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity style={styles.itemButton} onPress={()=>{
+        handleDelete()
+      }} activeOpacity={0.8}>
+        <Text style={styles.buttonText}>Delete</Text>
+      </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +41,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer:{
+
+    borderBottomWidth:1,
+    borderBottomColor:'#1a759f',
+    paddingHorizontal:8,
+    paddingVertical:16,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center'
+    
+  },
+  itemText:{
+    fontSize:18,
+      fontWeight:'bold',
+      
+  },
+  itemButton:{
+    backgroundColor: theme.colorBlack,
+    padding:8,
+    borderRadius:8
+  },
+  buttonText:{
+    color:theme.colorWhite,
+    fontWeight:'bold',
+    textTransform:'uppercase',
+    letterSpacing:1
+  }
 });
---
+++ theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+    colorCerulean: '#1a759f',
+    colorBlack: '#000000',
+    colorWhite: '#ffffff',
+};
