f2dd4913d8b93eef3e66d06d273776ed46988b07
Himanshu Rai <himanshuraimau9@gmail.com>
2025-01-14 22:08:18 +0530

    refactor: enhance countdown feature with notifications and confetti effects

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,55 +1,166 @@
-import React, { useEffect } from "react";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  Dimensions,
+} from "react-native";
+import React from "react";
+import { theme } from "../../theme";
 import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 import * as Notifications from "expo-notifications";
-import * as Device from "expo-device";
+import { useEffect, useRef, useState } from "react";
+import { intervalToDuration, isBefore } from "date-fns";
+import { TimeSegment } from "../../components/TimeSegment";
+import { getFromStorage, saveToStorage } from "../../utils/storage";
+import * as Haptics from "expo-haptics";
+import ConfettiCannon from "react-native-confetti-cannon";
 
-// Configure notification handler
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: true,
-  }),
-});
+// 2 weeks in ms
+const frequency = 14 * 24 * 60 * 60 * 1000;
+
+export const countdownStorageKey = "taskly-countdown";
+
+export type PersistedCountdownState = {
+  currentNotificationId: string | undefined;
+  completedAtTimestamps: number[];
+};
+
+type CountdownStatus = {
+  isOverdue: boolean;
+  distance: ReturnType<typeof intervalToDuration>;
+};
 
 export default function CounterScreen() {
-  useEffect(() => {
-    // Add notification listener
-    const subscription = Notifications.addNotificationReceivedListener(notification => {
-      console.log('Notification received:', notification);
-    });
+  const confettiRef = useRef<any>();
+  const [countdownState, setCountdownState] =
+    useState<PersistedCountdownState>();
+  const [status, setStatus] = useState<CountdownStatus>({
+    isOverdue: false,
+    distance: {},
+  });
 
-    return () => {
-      subscription.remove();
+  useEffect(() => {
+    const init = async () => {
+      const value = await getFromStorage(countdownStorageKey);
+      setCountdownState(value);
     };
+    init();
   }, []);
 
-  const handleRequestPermission = async () => {
+  const lastCompletedAt = countdownState?.completedAtTimestamps[0];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const timestamp = lastCompletedAt
+        ? lastCompletedAt + frequency
+        : Date.now();
+      const isOverdue = isBefore(timestamp, Date.now());
+
+      const distance = intervalToDuration(
+        isOverdue
+          ? { end: Date.now(), start: timestamp }
+          : { start: Date.now(), end: timestamp },
+      );
+
+      setStatus({ isOverdue, distance });
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [lastCompletedAt]);
+
+  const scheduleNotification = async () => {
+    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+    confettiRef?.current?.start();
+    let pushNotificationId;
     const result = await registerForPushNotificationsAsync();
     if (result === "granted") {
-      await Notifications.scheduleNotificationAsync({
+      pushNotificationId = await Notifications.scheduleNotificationAsync({
         content: {
-          title: "You've got mail! 📬",
-          body: "Here is the notification body",
-          data: { data: "goes here" },
+          title: "Car wash overdue!",
         },
         trigger: {
-          seconds: 5, // 5 minutes = 300 seconds
-          repeats: true
+          seconds: frequency / 1000,
         },
       });
     } else {
-      alert("Permission denied");
+      Alert.alert(
+        "Unable to schedule notification",
+        "Enable the notifications permission for Expo Go in settings",
+      );
     }
-  }
+
+    if (countdownState?.currentNotificationId) {
+      await Notifications.cancelScheduledNotificationAsync(
+        countdownState.currentNotificationId,
+      );
+    }
+
+    const newCountdownState: PersistedCountdownState = {
+      currentNotificationId: pushNotificationId,
+      completedAtTimestamps: countdownState
+        ? [Date.now(), ...countdownState.completedAtTimestamps]
+        : [Date.now()],
+    };
+
+    setCountdownState(newCountdownState);
+
+    await saveToStorage(countdownStorageKey, newCountdownState);
+  };
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} activeOpacity={0.8} onPress={handleRequestPermission}>
-        <Text style={styles.buttonText}>Schedule Notification</Text>
+    <View
+      style={[
+        styles.container,
+        status.isOverdue ? styles.containerLate : undefined,
+      ]}
+    >
+      {!status.isOverdue ? (
+        <Text style={[styles.heading]}>Next car wash due in</Text>
+      ) : (
+        <Text style={[styles.heading, styles.whiteText]}>
+          Car wash overdue by
+        </Text>
+      )}
+      <View style={styles.row}>
+        <TimeSegment
+          unit="Days"
+          number={status.distance?.days ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Hours"
+          number={status.distance?.hours ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Minutes"
+          number={status.distance?.minutes ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+        <TimeSegment
+          unit="Seconds"
+          number={status.distance?.seconds ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        />
+      </View>
+      <TouchableOpacity
+        onPress={scheduleNotification}
+        style={styles.button}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.buttonText}>I've washed the car!</Text>
       </TouchableOpacity>
-
+      <ConfettiCannon
+        ref={confettiRef}
+        count={50}
+        origin={{ x: Dimensions.get("window").width / 2, y: -30 }}
+        autoStart={false}
+        fadeOut={true}
+      />
     </View>
   );
 }
@@ -59,21 +170,33 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: theme.colorWhite,
   },
   button: {
-    backgroundColor: "black",
-    padding: 20,
-    borderRadius: 5,
+    backgroundColor: theme.colorBlack,
+    padding: 12,
+    borderRadius: 6,
   },
   buttonText: {
-    color: "white",
-    fontSize: 12,
+    color: "#fff",
     fontWeight: "bold",
-    letterSpacing: 1,
     textTransform: "uppercase",
+    letterSpacing: 1,
   },
-  text: {
+  row: {
+    flexDirection: "row",
+    marginBottom: 24,
+  },
+  heading: {
     fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 24,
+    color: theme.colorBlack,
+  },
+  containerLate: {
+    backgroundColor: theme.colorRed,
+  },
+  whiteText: {
+    color: theme.colorWhite,
   },
 });
